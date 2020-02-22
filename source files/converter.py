757951b2c09b03126ed5077cd2599b2380450bcf
MHindermann <50413752+MHindermann@users.noreply.github.com>
2020-02-22 20:47:47 +0100

    house keeping

--- source files/converter.py
+++ source files/converter.py
@@ -6,8 +6,28 @@ from collections import OrderedDict
 import json
 from os import path
 
+def load_codes(workbook) -> List[str]:
+    """ Load all codes from workbook """
+
+    codes = []
+    wb = load_workbook(workbook)
+    for ws in wb:
+        for row in ws.iter_rows(min_row=7, min_col=1, max_col=1, values_only=True):
+            code = row[0]
+            if code is None:
+                continue
+            else:
+                code = code.replace(" ", "")
+                codes.append(code)
+    return codes
+
+
 DIR = path.dirname(path.abspath(__file__))
 WORKBOOK = path.join(DIR, "OWC_Text.xlsx")
+CODES = load_codes(WORKBOOK)
+SCHEME = {"uri": "https://bartoc.org/owc/",
+          "type": "skos:ConceptScheme",
+          "label": "OWC Geographical Divisions"}
 
 
 def main(workbook: str) -> None:
@@ -16,25 +36,22 @@ def main(workbook: str) -> None:
     convert(workbook)
 
 
+def load_template() -> OrderedDict:
+    """ Load template """
+
+    with open("template.json", 'r') as file:
+        template = json.load(file)
+    vocabulary = OrderedDict()
+    vocabulary.update(template)
+    return vocabulary
+
+
 def convert(workbook: str) -> None:
     """ Convert workbook to JSON-LD in SKOS format """
 
-    # load codes:
-    codes = load_codes(workbook)
+    vocabulary = load_template()
+    graph = [SCHEME]
 
-    # load template and transform into ordered dict:
-    with open("template.json", 'r') as file:
-        template = json.load(file)
-    skos = OrderedDict()
-    skos.update(template)
-
-    concept_scheme = {"uri": "https://bartoc.org/owc/",
-                      "type": "skos:ConceptScheme",
-                      "label": "OWC Geographical Divisions"}
-
-    graph = [concept_scheme]
-
-    #
     wb = load_workbook(workbook)
     for ws in wb:
         for row in ws.iter_rows(min_row=3497, min_col=1, max_col=3, max_row=3508, values_only=True):  # max_row for dev
@@ -55,37 +72,22 @@ def convert(workbook: str) -> None:
             entry.update(labels)
 
             # add hierarchy:
-            entry.update(make_hierarchy(code, codes))
+            entry.update(make_hierarchy(code))
 
             graph.append(entry)
 
-    skos.update({"graph": graph})
+    vocabulary.update({"graph": graph})
 
     # print for debug
-    print(json.dumps(skos, indent=4, sort_keys=False))
+    print(json.dumps(vocabulary, indent=4, sort_keys=False))
 
-    return skos
-
-
-def load_codes(workbook) -> List[str]:
-    """ Load all codes from workbook """
-
-    codes = []
-    wb = load_workbook(workbook)
-    for ws in wb:
-        for row in ws.iter_rows(min_row=7, min_col=1, max_col=1, values_only=True):
-            code = row[0]
-            if code is None:
-                continue
-            else:
-                code = code.replace(" ", "")
-                codes.append(code)
-    return codes
+    return vocabulary
 
 
-def make_top(code: str, codes: List[str]) -> Dict:
+def make_top(code: str) -> Dict:
     """ Make hierarchy for a top concept """
 
+    codes = CODES.copy()
     broader = None
     narrower = []
     for entry in codes:
@@ -100,67 +102,91 @@ def make_top(code: str, codes: List[str]) -> Dict:
             # second symbol is not number and no other numbers (e.g., AA):
             elif len(entry) < 3:
                 narrower.append({"uri": make_uri(entry)})
-    hierarchy = {"broader": broader,
-                 "narrower": narrower}
-    return hierarchy
+    return {"broader": broader,
+            "narrower": narrower}
 
-def make_hierarchy(code: str, codes: List[str]) -> Dict:
-    """ Make broader and narrower for code """
 
-    # top concept:
-    if len(code) == 1:
-        return make_top(code, codes)
+def make_middle(code: str, debug: int = 0) -> Dict:
+    """ Make hierarchy for a middle concept """
 
-    # middle concept:
-    elif len(set(code).intersection(str(set(range(0, 10))))) == 0:
-        print(f"middle concept with code {code}")  # debug
-        broader = [{"uri": make_uri(code[0])}]
+    if debug == 1:
+        print(f"middle concept with code {code}")
+    codes = CODES.copy()
+    broader = [{"uri": make_uri(code[0])}]
+    narrower = []
+    for entry in codes:
+        if entry == code:
+            continue
+        elif code in entry and "." not in entry:
+            narrower.append({"uri": make_uri(entry)})
+    return {"broader": broader,
+            "narrower": narrower}
+
+
+def make_ojn_dot(code: str) -> Dict:
+    """ Make hierarchy for OJn-dot(-dot) concepts (e.g., OJ5.11 or OJ5.11.cAbau) """
+
+    codes = CODES.copy()
+    # OJ-dot-dot concept (e.g., OJ5.11.cAbau):
+    if code.count(".") == 2:
+        narrower = None
+        broader = [{"uri": make_uri(code.split(".")[0] + "." + code.split(".")[1])}]
+    # OJ-dot concept (e.g, OJ5.11):
+    else:
+        broader = [{"uri": make_uri(code.split(".")[0])}]
         narrower = []
         for entry in codes:
             if entry == code:
                 continue
-            elif code in entry and "." not in entry:
+            elif code in entry:
                 narrower.append({"uri": make_uri(entry)})
+        if len(narrower) == 0:
+            narrower = None
+    return {"broader": broader,
+            "narrower": narrower}
 
+
+def make_ojn(code: str) -> Dict:
+    """ Make hierarchy for OJn concepts (e.g., OJ1) """
+
+    codes = CODES.copy()
+    broader = [{"uri": make_uri("OJ")}]
+    narrower = []
+    for entry in codes:
+        if entry == code:
+            continue
+        elif code in entry and entry.count(".") == 1:
+            narrower.append({"uri": make_uri(entry)})
+    if len(narrower) == 0:
+        narrower = None
+    return {"broader": broader,
+            "narrower": narrower}
+
+
+def make_hierarchy(code: str) -> Dict:
+    """ Make broader and narrower for code """
+
+    # top concept:
+    if len(code) == 1:
+        return make_top(code)
+    # middle concept:
+    elif len(set(code).intersection(str(set(range(0, 10))))) == 0:
+        return make_middle(code)
     # bottom concept:
     else:
         print(f"bottom concept with code {code}")  # debug
-        # OJ as special case:
+        # OJn special case:
         if "." in code:
-            # bottom concept (e.g., OJ5.11.cAbau):
-            if code.count(".") == 2:
-                narrower = None
-                broader = [{"uri": make_uri(code.split(".")[0] + "." + code.split(".")[1])}]
-            # middle concept (e.g, OJ5.11):
-            else:
-                broader = [{"uri": make_uri(code.split(".")[0])}]
-                narrower = []
-                for entry in codes:
-                    if entry == code:
-                        continue
-                    elif code in entry:
-                        narrower.append({"uri": make_uri(entry)})
-                if len(narrower) == 0:
-                    narrower = None
-        elif "OJ" in code:
-            broader = [{"uri": make_uri("OJ")}]
-            narrower = []
-            for entry in codes:
-                if entry == code:
-                    continue
-                elif code in entry and entry.count(".") == 1:
-                    narrower.append({"uri": make_uri(entry)})
-            if len(narrower) == 0:
-                narrower = None
+            return make_ojn_dot(code)
 
+        elif "OJ" in code:
+            return make_ojn(code)
+        # standard case:
         else:
             narrower = None
             broader = [{"uri": make_uri(code[:2])}]
-
-    hierarchy = {"broader": broader,
-                 "narrower": narrower}
-
-    return hierarchy
+            return {"broader": broader,
+                    "narrower": narrower}
 
 
 def make_uri(code: str) -> str:
@@ -194,7 +220,7 @@ def parse(label: str) -> Dict:
     alt_label = None  # text in klammern oder k, evtl eher hiddenLabel
 
     # inScheme:
-    in_scheme = None # wenn top label, dann name des schemas
+    in_scheme = None  # wenn top label, dann name des schemas
 
     labels = {"prefLabel": pref_label,
               "altLabel": alt_label,
